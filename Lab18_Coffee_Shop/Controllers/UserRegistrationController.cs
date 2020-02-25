0f2745823c58d1e091a9f64e70e2a05839824a45
Mina <stanton.mina@gmail.com>
2020-02-24 20:53:37 -0500

    First commit for coffee shop lab

---
+++ Lab18_Coffee_Shop/Controllers/UserRegistrationController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab18_Coffee_Shop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab18_Coffee_Shop.Controllers
+{
+    public class UserRegistrationController : Controller
+    {
+        public IActionResult RegistrationForm()
+        {
+            return View(new RegisterUser());//returning a new instance of this user
+        }
+        
+
+        public IActionResult DisplayUserInformation(RegisterUser user)
+        {
+            if (ModelState.IsValid)//if they filled out the form correctly then return the view
+            {
+              
+               
+                return View(user);
+            }
+            else
+            {//if they did not then return the information on the registration form the user filled out
+                return View("RegistrationForm", user);
+            }
+        }
+
+        //find out why it is not displaying the second form 
+        public IActionResult DisplayAllUserInformation(RegisterUser user)
+        {
+            return View(user);
+        }
+
+    }
+}
---
+++ Lab18_Coffee_Shop/Models/RegisterUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab18_Coffee_Shop.Models
+{
+    public class RegisterUser
+    {
+        [Required]
+        [StringLength(20, MinimumLength =4, ErrorMessage ="Please enter a Username between 4 and 20 characters!")]
+        public string UserName { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9{5,30}]+@[a-zA-A0-9{5,10}]+\.[a-zA-Z0-9{2,3}]+$", ErrorMessage = "Please enter a valid  email address!")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(12, MinimumLength =6, ErrorMessage ="Please enter a password between 6 to 12 characters!")]
+        public string Password { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        public int Age { get; set; }
+
+        [RegularExpression(@"^\b\d{3}-\d{3}-\d{4}\b$", ErrorMessage = "Phone Number must be in correct format")]
+        public string PhoneNumber { get; set; }
+    }
+}
